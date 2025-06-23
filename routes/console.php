6d14f2905b83431995a6be8a31b8d27481b48f38
marcha <marcic74@gmail.com>
2025-06-23 06:22:51 +0200

    podesen scheduler

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('dmx:sync-profile etg_api_production')
+    ->everyFiveMinutes()
+    ->withoutOverlapping()
+    ->onOneServer(); // ako koristiš više servera
